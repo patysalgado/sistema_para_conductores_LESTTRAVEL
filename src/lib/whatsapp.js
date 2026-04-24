ac4c6134e90317efa5737d1849177f114e909e06
Paty Salgado <jp.salgadol@uea.edu.ec>
2026-04-23 20:33:56 -0500

    Correccion de librerias para navegador

--- src/lib/whatsapp.js
+++ src/lib/whatsapp.js
@@ -8,7 +8,15 @@ const initWhatsApp = () => {
     authStrategy: new LocalAuth(),
     puppeteer: {
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-extensions']
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--disable-gpu'
+      ]
     }
   });
 
